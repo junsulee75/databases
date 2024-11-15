2ac0982edbbcab37615a2cbc2761135d674b37da
Jun Su Lee <45049935+junsulee75@users.noreply.github.com>
2024-11-15 22:45:53 +1100

    mysql install start error fix

--- mysql/mysqlInstall.sh
+++ mysql/mysqlInstall.sh
@@ -16,13 +16,16 @@ esac
 
 
 installRepo(){
+	
+	print1 "mysql repository download : $MYSQL_YUMRPM "
 	wget https://dev.mysql.com/get/$MYSQL_YUMRPM
 	
 	if [ $? -ne 0 ]; then
-		echo "..mysql repository download failure =>  https://dev.mysql.com/get/$MYSQL_YUMRPM "
+		echo "..mysql repository download failure "
   		exit 1 
 	fi
 
+	print1 "Enabling mysql repos "
 	sudo yum -y install $MYSQL_YUMRPM
 	yum repolist enabled | grep "mysql.*-community.*"
 	yum repolist enabled | grep mysql
@@ -33,30 +36,39 @@ disableBuiltIn(){
 	sudo yum -y module disable mysql  # on Red Hat 8 only
 }
 
-installPostgre(){
+installDB(){
 	
-	disp_msglvl2 "Install mysql community "
+	disp_msglvl1 "Install mysql community "
 	sudo yum -y install mysql-community-server 
 }
 
 initDB(){
 	disp_msglvl1 "1st DB start "
 	## start
-	echo "Change policy to LOW... To use simple PW. Test system only"
-	echo "validate_password.policy = LOW" >> /etc/my.cnf  
+ 
 	systemctl start mysqld
 	systemctl status mysqld
+
+	print1 "Change policy to LOW... To use simple PW. Test system only"
+	print2 "Stopping mysqld"
+	systemctl stop mysqld
+	systemctl status mysqld
+	echo "validate_password.policy = LOW" >> /etc/my.cnf  # mysqld start issue if I do this before the 1st start.  
 	
-	disp_msglvl1 "Change the temp password to new PW and login"
+	print2 "Starting mysqld again"
+	systemctl start mysqld
+	systemctl status mysqld
+
+	print2 "Change the temp password to new PW and login to mysql"
 	NEWPW="passw0rd"
 	mysql -u root -p$(sudo grep 'temporary password' /var/log/mysqld.log |awk '{print $13;}') --connect-expired-password -v  -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$NEWPW'; "
 	mysql -u root -p${NEWPW}
 }
 
 
-pgStatus(){
+dbStatus(){
 	disp_msglvl2 "status"
-	systemctl status postgresql-${POSTGRE_VER}
+	systemctl status mysqld
 	disp_msglvl2 "processes"
 	ps -ef |grep post |grep -v grep
 	
@@ -65,6 +77,6 @@ pgStatus(){
 
 installRepo
 disableBuiltIn
-installPostgre
+installDB
 initDB
-pgStatus
+dbStatus
